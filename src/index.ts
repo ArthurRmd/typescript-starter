9553e0e99b83f27d31f838f9c8cf3c2b60637af3
Arthur <arthur.remond@protonmail.com>
2021-02-04 23:57:18 +0100

    add : dotenv 📦

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-const main = () => console.log('Typescript Template ♥‿♥')
+import { config as dotenv } from 'dotenv'
+dotenv()
+
+const main = () => {
+    console.log('Typescript Template ♥‿♥')
+    console.log('NODE_ENV = ' + process.env.NODE_ENV)
+}
 
 main()
-
